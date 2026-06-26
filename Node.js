eb79e2c471cc3b82d9b1493bfa376aa301ce405d
kawalepankaj <121873761+kawalepankaj@users.noreply.github.com>
2026-06-26 22:28:28 +0530

    Update Node.js

--- Node.js
+++ Node.js
@@ -4,6 +4,10 @@ const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  if (req.url === '/health') {
+    res.writeHead(200, {'Content-Type':'application/json'});
+    return res.end(JSON.stringify({status:'ok'}));
+  }
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
@@ -12,3 +16,7 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+process.on('SIGTERM', ()=>server.close(()=>process.exit(0)));
+process.on('SIGINT', ()=>server.close(()=>process.exit(0)));
